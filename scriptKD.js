10e01fc5a7db83af2f95e0db8dffcba896caab1d
lotte <Lottack@hotmail.com>
2018-10-19 11:35:02 +0200

    poging tot query

--- scriptKD.js
+++ scriptKD.js
@@ -1,11 +1,19 @@
 var genres = [];
 var activities = [];
+var queryRock = "PREFIX mu: <http://www.semanticweb.org/music/> SELECT ?s WHERE {?s mu:hasGenre mu:Rock}"
+var queryRockUri = "http://localhost:5820/music#!/query/" + '?query=' + encodeURIComponent(queryRock);
+var jsonpRockUri = 'http://morph.talis.com/?data-uri[]='+encodeURIComponent(queryRockUri)+'&output=jsonp&callback=processSparqlResults';
+var newScript = document.createElement('script');
+newScript.src=jsonpRockUri;
+document.body.appendChild(newScript);
+function processSparqlResults(data){  console.log(data);  }
 
 
 function CheckBoxes() {
   var x = document.getElementsByName("FormGenre");
   var i;
-
+  var genres = [];
+  var activities = [];
   for(i = 0; i < x.length; i++) {
       if (x[i].checked) {
           genres.push(x[i].value);
