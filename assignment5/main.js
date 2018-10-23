e9afec16ad8c88c0ae38e20220eb6a7a780e16b1
lotte <Lottack@hotmail.com>
2018-10-23 15:05:27 +0200

    allemaal aanpassingen

--- assignment5/main.js
+++ assignment5/main.js
@@ -1,37 +1,12 @@
 angular.module('KRRclass', [ 'chart.js']).controller('MainCtrl', ['$scope','$http', mainCtrl]);
 
-$scope.myAppName = "MoodMusic";
-function mainCtrl($scope, $http){
-
-	// TODO : type here code for your Ex 1
-
-
-
-	// TODO : type here code for your Ex 2
-
-
-
-	// TODO : type here code for your Ex 2
-	$scope.doMyAction = function(){
-
-
-	};
-
-}
-
-
-
-var genres = [];
-var activities = [];
-var queryRock = " SELECT ?s WHERE {?s http://localhost:5820/music#!/query/music/hasGenre mu:Rock}";
-
 
 
 function CheckBoxes() {
   var x = document.getElementsByName("FormGenre");
   var i;
-  var genres = [];
-  var activities = [];
+  //var genres = "";
+  genres = [];
   for(i = 0; i < x.length; i++) {
       if (x[i].checked) {
           genres.push(x[i].value);
@@ -44,12 +19,67 @@ function CheckBoxes() {
   var i;
   for(i = 0; i < activityList.length; i++) {
       if (activityList[i].checked) {
-          activities.push(activityList[i].value);
+          activities = (activityList[i].value);
 
           }
         }
 console.log(activities);
 }
+
+
+
+function mainCtrl($scope, $http, ChartJsProvider){
+
+	// TODO : type here code for your Ex 1
+    $scope.myAppName = "MoodMusic";
+    $scope.myAppList = [ "codaonto:Study", "codaonto:Treatment", "codaonto:Variable" ];
+    $scope.mysparqlendpoint = "http://localhost:5820/music/query/?query="
+
+	// TODO : type here code for your Ex 2
+
+
+
+	// TODO : type here code for your Ex 2
+  $scope.doMyAction = function(){
+        console.log('test');
+        $scope.result = "Here is my input: " +$scope.myInput+"!";
+        $scope.songs_playlist = [];
+        html = "<iframe src=\"https://open.spotify.com/embed/track/2zYzyRzz6pRmhPzyfMEC8s\" width=\"300\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
+        document.getElementById("songs").innerHTML = html;
+        var i;
+        if (genres.length == 0){
+          window.alert("please select genre(s)")
+        }
+        for (i = 0; i < genres.length; i++) {
+        $scope.dynamicQuery = "SELECT ?code WHERE { ?s <http://www.semanticweb.org/music/hasGenre> <http://www.semanticweb.org/music/"+genres[i]+"> . ?s <http://www.semanticweb.org/music/hasSpotifyCode> ?code }";
+        //$scope.dynamicQuery = $scope.myInput;
+
+
+        console.log($scope.mysparqlendpoint+encodeURI($scope.dynamicQuery).replace(/#/g, '%23'));
+        $http( {
+        method: "GET",
+        headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'},
+        url : $scope.mysparqlendpoint+encodeURI($scope.dynamicQuery).replace(/#/g, '%23'),
+
+    } )
+    .success(function(data, status ) {
+        console.log(data);
+        $scope.songs_playlist.push(data);
+        $scope.resultQ2=data;
+    })
+    .error(function(error ){
+        console.log('Error'+error);
+    });
+  }
+
+}
+console.log($scope.songs_playlist);
+
+
+}
+
+
+
 
 function myFunction() {
     var x = document.getElementById("player");
